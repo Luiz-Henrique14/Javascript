6baa1db9a85e84d87dc288a0062b5790c7d46fe2
Luiz Henrique Abreu dos Santos <luiz.henrique.dos.santos1402@gmail.com>
2026-03-15 17:33:56 -0300

    entendendo if e else

---
+++ ex008.js
@@ -0,0 +1,8 @@
+var vel = 60
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60){ //Condição Simples
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!!!`)
+}
+
+
+console.log(`Sempre dirija com sinto de segurança`)
